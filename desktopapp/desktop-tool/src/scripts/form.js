db377b83b025eb3bcd31e030ab8a1a351a0b9cd0
robertsgrausmanis <129615043+robertsgrausmanis@users.noreply.github.com>
2024-04-09 14:07:15 +0200

    Inloggnings form och registerings form
    Commitar inloggningsformen och registeringsformen, design färdig.

--- desktopapp/desktop-tool/src/scripts/form.js
+++ desktopapp/desktop-tool/src/scripts/form.js
@@ -1,25 +1,43 @@
-const { ipcRenderer } = require('electron');
+//const { ipcRenderer } = require('electron');
 
 form = document.getElementById('register-form');
 
 form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const formData = new FormData(form);
-    const formDataObject = {};
-    formData.forEach((value, key) => {
-        formDataObject[key] = value;
-    });
+    event.preventDefault(); // Prevent default button click behavior
 
-    ipcRenderer.send('register', formDataObject);
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    var confirmPassword = document.getElementById('confirm-password').value;
 
-});
-
-ipcRenderer.on('displayApiResponse', (event, responseData) => {
-    // Update HTML elements to display the response data
-    const responseContainer = document.getElementById('response-container');
-    if(responseData.hasOwnProperty("response")){
-        ipcRenderer.send('load-page', 'projectview.html');
+    if (email.trim() === "") {
+        alert("Email is required.");
+        return;
     }
 
+    if (password.trim() === "") {
+        alert("Password is required.");
+        return;
+    }
 
-  });
+    if (password !== confirmPassword) {
+        alert("Passwords do not match.");
+        return;
+    }
+console.log("success");
+    // If all validation checks pass, continue with form submission
+    const formData = {
+        email: email,
+        password: password,
+        confirmPassword: confirmPassword
+    };
+
+   // ipcRenderer.send('register', formData);
+});
+
+// ipcRenderer.on('displayApiResponse', (event, responseData) => {
+//     // Update HTML elements to display the response data
+//     const responseContainer = document.getElementById('response-container');
+//     if (responseData.hasOwnProperty("response")) {
+//         ipcRenderer.send('load-page', 'projectview.html');
+    //}
+//});
