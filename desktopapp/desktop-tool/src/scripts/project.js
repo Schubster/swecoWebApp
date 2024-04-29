1aeac68242fec93f76a051cd08fd1cdf846a6334
robertsgrausmanis <129615043+robertsgrausmanis@users.noreply.github.com>
2024-04-29 13:25:15 +0200

    uppdatering
    Redskap divvar

--- desktopapp/desktop-tool/src/scripts/project.js
+++ desktopapp/desktop-tool/src/scripts/project.js
@@ -5,3 +5,21 @@ document.querySelectorAll(".tab").forEach(tab => {
         document.querySelector("." + tab.dataset.container).classList.add('selected');
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Get all elements with class 'tooltab'
+    var tooltabs = document.querySelectorAll('.tooltab');
+
+    // Loop through each 'tooltab' element
+    tooltabs.forEach(function (tab) {
+        // Add click event listener to each 'tooltab'
+        tab.addEventListener('click', function () {
+            // Remove 'clicked' class from all 'tooltab' elements
+            tooltabs.forEach(function (t) {
+                t.classList.remove('clicked');
+            });
+            // Add 'clicked' class to the clicked 'tooltab' element
+            tab.classList.add('clicked');
+        });
+    });
+});
